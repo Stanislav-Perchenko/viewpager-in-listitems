923724030182831eb754b47a2c3398603453b80f
stanislav.perchenko <lkjnbv123>
2018-10-12 10:29:46 +0300

    Initial commit

---
+++ app/src/main/java/com/alperez/samples/pagedlistitem/MainActivity.java
@@ -0,0 +1,76 @@
+package com.alperez.samples.pagedlistitem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.alperez.samples.pagedlistitem.widget.ListItemView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final boolean D = BuildConfig.DEBUG;
+    public static final String LOG_TAG = "MY_ADAPTER";
+
+    public static final int N_ITEMS = 13;
+    public static final int N_PAGES_IN_ITEM = 9;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((ListView) findViewById(android.R.id.list)).setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return N_ITEMS;
+            }
+
+            @Override
+            public String getItem(int i) {
+                return "Item #"+i;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return i;
+            }
+
+            private final int itemHeightPx = getResources().getDimensionPixelSize(R.dimen.list_item_height);
+            private final Map<Integer, Integer> savedPositions = new HashMap<>();
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (D) Log.e(LOG_TAG, "==========================   getView("+position+")   ===========================");
+                ListItemView row;
+                if (convertView == null) {
+                    if (D) Log.e(LOG_TAG, "    Build new View item");
+                    // Build new List item View. The internal ViewPager position will be set by default.
+                    row = new ListItemView(MainActivity.this, N_PAGES_IN_ITEM);
+                    row.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, itemHeightPx));
+                } else {
+                    row = (ListItemView) convertView;
+                    // Preserve previous item ViewPager position
+                    savedPositions.put(row.getPositionInList(), row.getPagerPosition());
+                    if (D) Log.d(LOG_TAG, String.format("    Got recycled View. oldListPosition=%d, oldPagerPosition=%d", row.getPositionInList(), row.getPagerPosition()));
+                }
+                row.setPositionInList(position);
+                row.setTextData(getItem(position));
+                Integer pagePos = savedPositions.get(position);
+                if (pagePos != null) {
+                    row.setPagerPosition(pagePos);
+                    if (D) Log.d(LOG_TAG, "    --> Restore pager position - "+pagePos);
+                } else {
+                    row.setDefaultPagerPosition();
+                    if (D) Log.d(LOG_TAG, "    ~~> Set default pager position");
+                }
+                return row;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/alperez/samples/pagedlistitem/widget/ListItemView.java
@@ -0,0 +1,70 @@
+package com.alperez.samples.pagedlistitem.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.alperez.samples.pagedlistitem.MyItemAdapter;
+import com.alperez.samples.pagedlistitem.R;
+import com.alperez.widget.CirclePageIndicator;
+
+/**
+ * Created by stanislav.perchenko on 10/11/2018
+ */
+public class ListItemView extends FrameLayout {
+
+    private final int nPages;
+    private String textData;
+    private int positionInList;
+
+    private TextView vTxtTitle;
+    private ViewPager vPager;
+    private CirclePageIndicator vPageIndicator;
+
+    public ListItemView(@NonNull Context context, int nPages) {
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.view_list_item, this, true);
+
+        vTxtTitle = (TextView) findViewById(R.id.txt_title);
+        (vPager = (ViewPager) findViewById(R.id.pager)).setAdapter(new MyItemAdapter(this.nPages = nPages));
+        (vPageIndicator = (CirclePageIndicator) findViewById(R.id.page_indicator)).setViewPager(vPager, nPages / 2);
+    }
+
+    public void setDefaultPagerPosition() {
+        vPageIndicator.setCurrentItemNoAnimation(nPages / 2);
+    }
+
+    public void setPagerPosition(int position) {
+        if (position < 0 || position >= nPages) {
+            throw new IllegalArgumentException("Wrong position value - "+position+", N pages = "+nPages);
+        } else {
+            vPageIndicator.setCurrentItemNoAnimation(position);
+        }
+    }
+
+    public int getPagerPosition() {
+        return vPager.getCurrentItem();
+    }
+
+
+
+    public void setTextData(String textData) {
+        if (!TextUtils.equals(this.textData, textData)) {
+            vTxtTitle.setText(this.textData = textData);
+            ((MyItemAdapter) vPager.getAdapter()).setTextData(textData);
+            vPager.getAdapter().notifyDataSetChanged();
+        }
+    }
+
+    public int getPositionInList() {
+        return positionInList;
+    }
+
+    public void setPositionInList(int positionInList) {
+        this.positionInList = positionInList;
+    }
+}
